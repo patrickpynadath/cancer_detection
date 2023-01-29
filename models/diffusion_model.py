d0ba9989b6ab60ee917a285ce575ed2a3803b650
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-01-29 13:18:02 -0600

    added diffusion training to command line interface

--- models/diffusion_model.py
+++ models/diffusion_model.py
@@ -19,6 +19,34 @@ def get_diffusion_model_from_args(args):
 
 # the out-the-box diffusion only produces square images, but its pretty simple to adjust that
 class GaussianDiffusionCustom(GaussianDiffusion):
+    def __init__(self,
+                model,
+                *,
+                image_size,
+                timesteps = 1000,
+                sampling_timesteps = None,
+                loss_type = 'l1',
+                objective = 'pred_noise',
+                beta_schedule = 'sigmoid',
+                schedule_fn_kwargs = dict(),
+                p2_loss_weight_gamma = 0., # p2 loss weight, from https://arxiv.org/abs/2204.00227 - 0 is equivalent to weight of 1 across time - 1. is recommended
+                p2_loss_weight_k = 1,
+                ddim_sampling_eta = 0.,
+                auto_normalize = True):
+        super().__init__(
+            model,
+            image_size = image_size,
+            timesteps = timesteps,
+            sampling_timesteps = sampling_timesteps,
+            loss_type = loss_type,
+            objective = objective,
+            beta_schedule = beta_schedule,
+            schedule_fn_kwargs = schedule_fn_kwargs,
+            p2_loss_weight_gamma = p2_loss_weight_gamma, # p2 loss weight, from https://arxiv.org/abs/2204.00227 - 0 is equivalent to weight of 1 across time - 1. is recommended
+            p2_loss_weight_k = p2_loss_weight_k,
+            ddim_sampling_eta = ddim_sampling_eta,
+            auto_normalize = auto_normalize)
+
     @torch.no_grad()
     def sample(self, batch_size = 16, return_all_timesteps = False):
         image_size, channels = self.image_size, self.channels

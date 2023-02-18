5aedd3e1ce21b32ef25fed1799850f759017f272
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-18 17:58:32 -0600

    qual values cause NAN errors! commenting out to see if rest works

--- models/decoder.py
+++ models/decoder.py
@@ -24,18 +24,18 @@ class Decoder(nn.Module):
 
         self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens,
                                                 out_channels=num_hiddens,
-                                                kernel_size=2,
+                                                kernel_size=3,
                                                 stride=1, padding=0)
 
         self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens,
                                                 out_channels=num_hiddens // 2,
-                                                kernel_size=2,
-                                                stride=1, padding=1)
+                                                kernel_size=4,
+                                                stride=2, padding=1)
 
         self._conv_trans_3 = nn.ConvTranspose2d(in_channels=num_hiddens //2,
                                                 out_channels=out_channels,
                                                 kernel_size=4,
-                                                stride=2, padding=1)
+                                                stride=1, padding=1)
 
 
 

fa54a712dcbc2fd95bb9345a54f97c26244a7736
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-18 14:48:22 -0600

    debugged and ready to go

--- models/ae.py
+++ models/ae.py
@@ -47,7 +47,6 @@ class PLAutoEncoder(pl.LightningModule):
 
     def decode(self, z):
         dec = self._fc_dec(z)
-        dec = nn.functional.relu(dec)
         pre_recon = dec.view(-1, self.enc_dim[1], self.enc_dim[2], self.enc_dim[3])
         x_recon = self._decoder(pre_recon)
         return x_recon

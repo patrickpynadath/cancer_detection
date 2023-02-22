ff105edfdea815f35d891139c49bdf13bbf12a18
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-22 12:41:00 -0600

    cleaned up main.py, moved all arg config to new package for clarity

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,4 @@ from .resnet_clf import ResNet, PLResNet, resnet_from_args
 from .normalization_layers import get_normalize_layer
 from .diffusion_model import GaussianDiffusionCustom, get_diffusion_model_from_args, get_trained_diff_model, create_save_artificial_samples
 from .windowed_model import WindowModel, EnsembleModel, get_window_model
-from .ae import get_pl_ae
+from .ae import get_pl_ae, PLAutoEncoder

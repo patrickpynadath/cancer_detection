a64870318d77a1e4262c1eee43817c97099a3954
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-23 14:54:32 -0600

    added package for imbalanced clf rl

--- imbalanced_rl_clf/imbclf_env.py
+++ imbalanced_rl_clf/imbclf_env.py
@@ -39,6 +39,7 @@ class ImbalancedClfEnv(gym.Env):
         terminated = not np.any(self.mask)
 
         if env_action == 1:
+            print('pos sample')
             self.num_pos_total += 1
             if action == env_action:
                 reward = 1

c06d648e0af89cd42041752a6b908ec4dc7768a2
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-23 22:29:05 -0600

    added batch norm to hopefully make training less bad

--- imbalanced_rl_clf/dqn.py
+++ imbalanced_rl_clf/dqn.py
@@ -8,14 +8,12 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         self.encoder = encoder
 
-        self.mp = nn.Sequential(nn.Linear(1024, 512),
-                                nn.ReLU(),
-                                nn.LazyBatchNorm1d(),
-                                nn.Linear(512, 256),
-                                nn.ReLU(),
-                                nn.LazyBatchNorm1d(),
-                                nn.Linear(256, 2),
-                                nn.ReLU())
+
+        self.l1 = nn.Linear(1024, 512)
+        self.l2 = nn.Linear(512, 256)
+        self.l3 = nn.Linear(512, 256)
+        self.l4 = nn.Linear(256, 2)
+        self.mp = nn.Sequential(self.l1, nn.ReLU(), self.l2, nn.ReLU(), self.l3, nn.ReLU(), self.l4)
         dummy = torch.zeros(size=(32, 1024))
         dummy = self.mp(dummy)
 

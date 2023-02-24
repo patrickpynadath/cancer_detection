0172747dcf05ddd04bc711d3b540b1b8153e7eed
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-23 21:42:17 -0600

    bug with naming

--- main.py
+++ main.py
@@ -121,6 +121,6 @@ if __name__ == '__main__':
         trained_jigsaw_ae.to(device)
         encoder = trained_jigsaw_ae.encode
         env = ImbalancedClfEnv(trainloader.dataset, device)
-        agent = Agent(2, 0.05, 0.9, 1000, encoder, device, 10000, 256, .01)
+        agent = Agent(2, 0.05, 0.9, 1000, encoder, device, 10000, 64, .01)
         trainer = RLTrainer(.99, .005, env, agent, device, test_loader)
         trainer.train_loop(150000)

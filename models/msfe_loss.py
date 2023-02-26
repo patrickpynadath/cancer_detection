8605a4e94b4ef2d842ef244542bb31a974e40fc5
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-26 17:13:07 -0600

    made new loss function

--- models/msfe_loss.py
+++ models/msfe_loss.py
@@ -19,5 +19,7 @@ class MSFELoss(_Loss):
         pos_idx = (target == 1).nonzero()
         FPE = F.mse_loss(input[pos_idx], new_target[pos_idx])
         FNE = F.mse_loss(input[neg_idx], new_target[neg_idx])
+        print(FPE)
+        print(FNE)
         l = FPE ** 2 + (FNE ** 2)
         return l

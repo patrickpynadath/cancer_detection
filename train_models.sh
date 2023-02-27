8761ad6cef6a2c971c6867563187e01d191cfd16
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-26 21:58:02 -0600

    added shell script for convenience

---
+++ train_models.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion CE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion MSFE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion CE --lr .00001  --track_grad_norm 2 --use_encoder_params True;
+python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion MSFE --lr .00001  --track_grad_norm 2 --use_encoder_params True;
+python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion MSFE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion MSFE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion CE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion CE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion MSFE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion CE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion MSFE --lr .00001  --track_grad_norm 2;
+python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 200 --criterion CE --lr .00001  --track_grad_norm 2;

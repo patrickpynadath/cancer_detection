c7a92578c0a56c2eda0488b139c02e4f669019ee
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-28 10:07:43 -0600

    chagned dim to (256, 128) for final tests

--- train_models.sh
+++ train_models.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion CE  --track_grad_norm 2 --auto_lr_find True;

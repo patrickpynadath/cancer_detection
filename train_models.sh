63c91dbb83387d33843c3930b408976506d51f43
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-28 07:35:50 -0600

    chagned dim to (256, 128) for final tests

--- train_models.sh
+++ train_models.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion CE --track_grad_norm 2 --auto_lr_find True;
 python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion MSFE --lr .00001  --track_grad_norm 2;
-python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion CE --track_grad_norm 2 --use_encoder_params True --auto_lr_find True;
-python main.py train_clf --sample_strat none  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion MSFE --lr .00001  --track_grad_norm 2 --use_encoder_params True;
 python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion CE  --track_grad_norm 2 --auto_lr_find True;
@@ -10,4 +7,4 @@ python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerato
 python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion CE  --track_grad_norm 2 --auto_lr_find True;
 python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion MSFE --lr .00001  --track_grad_norm 2;
-python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion CE --track_grad_norm 2 --auto_lr_find True;
+python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 100 --criterion CE --track_grad_norm 2 --lr .00001 ;

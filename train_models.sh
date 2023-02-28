4243dddd2782238100ddfb8aa9f1f737724ba9cf
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-28 13:22:40 -0600

    chagned dim to (256, 128) for final tests

--- train_models.sh
+++ train_models.sh
@@ -3,7 +3,5 @@ python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerato
 python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2;
 python main.py train_clf --sample_strat ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion CE  --track_grad_norm 2 --auto_lr_find True;
 python main.py train_clf --sample_strat rus  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion CE  --track_grad_norm 2 --auto_lr_find True;
-python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2;
-python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion CE  --track_grad_norm 2 --auto_lr_find True;
-python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2;
-python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion CE --track_grad_norm 2 --lr .00001 ;
+python main.py train_clf --sample_strat dynamic_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2 --balancing_beta .5;
+python main.py train_clf --sample_strat dynamic_kmeans_ros  --learning_mode jigsaw --accelerator gpu --devices 1 --device cuda --max_epochs 50 --criterion MSFE --lr .00001  --track_grad_norm 2 --balancing_beta .5;
--- training/training_loops.py
+++ training/training_loops.py
@@ -8,9 +8,9 @@ def generic_training_loop(args,
                           pl_model,
                           train_loader,
                           val_loader,
-                          model_name,
-                          dynamic = False):
+                          model_name):
     tb_logger = TensorBoardLogger(save_dir='lightning_logs', name=model_name)
+    # getting the checkpoint dir
     trainer = pl.Trainer.from_argparse_args(args, logger=tb_logger, num_sanity_val_steps=0)
     trainer.tune(pl_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
     trainer.fit(pl_model, train_loader, val_loader)

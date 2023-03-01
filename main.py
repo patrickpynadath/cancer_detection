d06700aaa3696c5de63cbef0c52836fc848b6928
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-02-28 21:45:19 -0500

    chagned dim to (256, 128) for final tests

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
                                              use_encoder_params=args.use_encoder_params,
                                              criterion=criterion)
             trainer.training_loop(args.epochs)
-            torch.save(mlp.state_dict(), f'{args.sample_strat}/model_sd.pickle')
+            torch.save(mlp.state_dict(), f'{args.sample_strat}_model_sd.pickle')
 
         else:
             clf = PL_MLP_clf(mlp, criterion, args.lr, use_encoder_params=args.use_encoder_params)
